4a6f64546da2a5c32167b63d350c1270ff593659
gsihaj5 <gsihaj5@gmail.com>
2021-12-08 21:03:25 +0700

    feat: camera now start at starting point

--- main.js
+++ main.js
@@ -7,52 +7,49 @@ let camera, scene, renderer
 init()
 animate()
 
-function init () {
-	scene = new THREE.Scene()
-	initCamera()
-	initRenderer()
-	initLight()
+function init() {
+    scene = new THREE.Scene()
+    initCamera()
+    initRenderer()
+    initLight()
 
-	let control = new MovementControl(camera, renderer.domElement)
+    let control = new MovementControl(camera, renderer.domElement)
 
-	new Room( scene)
-
+    new Room(scene)
 }
 
-function initLight () {
-	let light = new THREE.PointLight(0x404040)
-	light.position.set(5, 5, 5)
-	scene.add(light)
+function initLight() {
+    let light = new THREE.PointLight(0x404040)
+    light.position.set(5, 5, 5)
+    scene.add(light)
 
-	let ambient = new THREE.AmbientLight(0x404040)
-	scene.add(ambient)
-
+    let ambient = new THREE.AmbientLight(0x404040)
+    scene.add(ambient)
 }
 
-function initRenderer () {
-	renderer = new THREE.WebGLRenderer()
-	renderer.setPixelRatio(window.devicePixelRatio)
-	renderer.setSize(window.innerWidth, window.innerHeight)
-	container.appendChild(renderer.domElement)
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer()
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    container.appendChild(renderer.domElement)
 }
 
-function initCamera () {
-	camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1100,
-	)
-	camera.position.set(0, 0, 5)
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1100
+    )
+    camera.position.set(0, 0, -25)
+    camera.rotation.y += Math.PI
 }
 
-function update () {
-
-	renderer.render(scene, camera)
+function update() {
+    renderer.render(scene, camera)
 }
 
-function animate () {
-	update()
-	requestAnimationFrame(animate)
+function animate() {
+    update()
+    requestAnimationFrame(animate)
 }
-
