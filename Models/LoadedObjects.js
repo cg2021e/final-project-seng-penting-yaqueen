22bbb39afc52bb233733b91c628a39b230140d23
Gerry Sihaj <gsihaj5@gmail.com>
2021-12-08 13:27:29 +0700

    feat: add onLoad method inside Loaded Objects

--- Models/LoadedObjects.js
+++ Models/LoadedObjects.js
@@ -1,10 +1,12 @@
 export default class LoadedObjects {
-	constructor (modelPath, scene) {
+	constructor (modelPath, scene, onLoad) {
 		this.loader = new THREE.GLTFLoader()
 
 		this.loader.load('./GLTF/models.glb',
 			(loadedGLTF) => {
-				scene.add(loadedGLTF.scene)
+				this.object = loadedGLTF.scene
+				scene.add(this.object)
+				onLoad()
 			})
 	}
 }
