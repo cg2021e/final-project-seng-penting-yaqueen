8b0ed95298f293b36b837124be8868e43772453c
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2021-12-14 20:16:00 +0700

    Fix : Disable movement when not focused

--- MovementControl.js
+++ MovementControl.js
@@ -1,37 +1,46 @@
 export default class MovementControl {
-	constructor (camera, domElement) {
-		const startButton = document.getElementById('startButton')
-		let menu = document.getElementById('menuPanel')
-		let control = new THREE.PointerLockControls(camera, domElement)
-		startButton.addEventListener(
-			'click',
-			function () {
-				control.lock()
-			},
-			false
-		)
-		// menu.style.display = 'none'
+    constructor(camera, domElement) {
+        let movementLocked = true
+        const startButton = document.getElementById('startButton')
+        let menu = document.getElementById('menuPanel')
+        let control = new THREE.PointerLockControls(camera, domElement)
+        startButton.addEventListener(
+            'click',
+            function () {
+                control.lock()
+            },
+            false
+        )
+        // menu.style.display = 'none'
 
-		control.addEventListener('lock', function () {
-			menu.style.display = 'none'
-		})
+        control.addEventListener('lock', function () {
+            movementLocked = false;
+            menu.style.display = 'none'
+        })
 
-		control.addEventListener('unlock', function () {
-			menu.style.display = 'block'
-		})
+        control.addEventListener('unlock', function () {
+            movementLocked = true;
+            menu.style.display = 'block'
+        })
 
-		const onKeyDown = function (event) {
-			if (event.code == 'KeyW') {
-				control.moveForward(.125)
-			} else if (event.code == 'KeyA') {
-				control.moveRight(-.125)
-			} else if (event.code == 'KeyS') {
-				control.moveForward(-.125)
-			} else if (event.code == 'KeyD') {
-				control.moveRight(.125)
-			}
-		}
-		document.addEventListener('keydown', onKeyDown, false)
-	}
+        const onKeyDown = function (event) {
+            if (!movementLocked) {
+                if (event.code == 'KeyW') {
+                    control.moveForward(.125)
+                }
+                if (event.code == 'KeyA') {
+                    control.moveRight(-.125)
+                }
+                if (event.code == 'KeyS') {
+                    control.moveForward(-.125)
+                }
+                if (event.code == 'KeyD') {
+                    control.moveRight(.125)
+                }
+            }
+        }
+
+        document.addEventListener('keydown', onKeyDown, false)
+    }
 
 }
