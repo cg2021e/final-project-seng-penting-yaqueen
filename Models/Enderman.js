7087ee8ae9511a867edbcebb2b04fa1434f02195
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2021-12-15 20:58:32 +0700

    Fix : change enderman rotation

--- Models/Enderman.js
+++ Models/Enderman.js
@@ -2,24 +2,24 @@ import * as THREE from 'three'
 import LoadedObjects from './LoadedObjects.js'
 
 export default class Enderman extends LoadedObjects {
-    constructor (scene) {
-		super('./GLTF/enderman.glb', scene)
-		this.mixers = []
-	}
+    constructor(scene) {
+        super('./GLTF/enderman.glb', scene)
+        this.mixers = []
+    }
 
-    load () {
-		super.load().then(() => {
-			this.scene.add(this.object)
+    load() {
+        super.load().then(() => {
+            this.scene.add(this.object)
 
-			this.animations.forEach((clip) => {
-				let mixer = new THREE.AnimationMixer(this.object)
-				this.mixers.push(mixer)
-				mixer.clipAction(clip).play()
-			})
+            this.animations.forEach((clip) => {
+                let mixer = new THREE.AnimationMixer(this.object)
+                this.mixers.push(mixer)
+                mixer.clipAction(clip).play()
+            })
 
-			this.object.scale.set(.4, .4, .4)
-			this.object.rotation.y -= Math.PI
-			this.object.position.set(0, -1, 6)
-		})
-	}
+            this.object.scale.set(.4, .4, .4)
+            this.object.rotation.y += 1 / 2 * Math.PI
+            this.object.position.set(0, -1, 6)
+        })
+    }
 }
